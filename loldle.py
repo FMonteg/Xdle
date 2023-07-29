abe7b24bc42efe5b9a2a9e08d4c325ddc972f4f4
FMonteg <fabien.montegut@gmail.com>
2023-07-29 17:25:20 +0200

    start of Loldle, frist half database

---
+++ loldle.py
@@ -0,0 +1,163 @@
+########## To Do ##############
+#
+# Complete the database
+# Format data
+# Test
+
+
+########## Imports ############
+
+import pandas as pd
+import math
+
+
+########## Classes ############
+
+class Loldle_Game():
+    def __init__(self, database **kwargs):
+        self.states = []
+        self.possible = range(database.shape[0])
+        self.entropy = 0
+        pass
+
+    def update(self, database):
+        state = []
+
+        print('Champion proposé :')
+        name = input()
+        state.append(name)
+
+        print('Genre (r/o/g) :')
+        state.append(input())
+
+        print('Rôle (r/o/g) :')
+        state.append(input())
+
+        print('Espèce (r/o/g) :')
+        state.append(input())
+
+        print('Ressource (r/o/g) :')
+        state.append(input())
+
+        print('Portée (r/o/g) :')
+        state.append(input())
+
+        print('Régions (r/o/g) :')
+        state.append(input())
+
+        print('Année (+/=/-):')
+        state.append(input())
+
+        state = tuple(state)
+        self.states.append(state)
+
+        remaining=[]
+        stats_hint = database.iloc[database[database['Nom'] == name].index[0]]
+        for i in self.possible:
+            stats_test = database.iloc[i]
+            if self.compatible(state, stats_hint, stats_test):
+                remaining.append(i)
+        self.possible = remaining
+        pass
+
+    def compatible(self, hint, stats_hint, stats_test):
+        columns_std = ['Genre', 'Rôle', 'Espèce', 'Ressource', 'Portée', 'Régions']
+
+        #Compare each characteristic
+        for i in range(len(columns_std)):
+            T = set(stats_hint[columns_std[i]])
+            R = set(stats_test[columns_std[i]])
+            if hint[i]=='g' and T!=R:
+                return False
+            elif hint[i]=='r' and len(set.intersection(T,R))>0:
+                return False
+            elif hint[i]=='o' and (T==R or len(set.intersection(T,R))==0):
+                return False
+
+        
+        #Compare year
+        if hint[-1]=="=" and stats_hint['Année']!=stats_test['Année']:
+            return False
+        elif hint[-1]=="+" and stats_hint['Année']>stats_test['Année']:
+            return False
+        elif hint[-1]=="-" and stats_hint['Année']<stats_test['Année']:
+            return False
+        
+        return True
+
+
+    def suggest_play(self, database):
+
+        temp = database.iloc[self.possible]
+        entropy = pd.Series(index = temp['Nom'].to_list())
+
+        for i in self.possible:
+            stats_try = database.iloc[i]
+
+
+            counts = {}
+            for j in self.possible:
+                stats_real = database.iloc[j]
+
+                hint = self.compare(stats_try, stats_real)
+                counts[hint] = counts.get(hint, 0) + 1
+            
+            calcul = []
+            total = len(self.possible)
+            for hint in counts.keys():
+                p = counts[hint]/total
+                log = math.log(1/p, 2)
+                calcul.append(p*log)
+            entropy.loc[temp.loc[i,'Nom']] = sum(calcul) + self.entropy
+
+        print("10 entropies les plus élevées :")
+        print(entropy.sort_values(ascending=False).head(10))
+
+        self.entropy += entropy.sort_values(ascending=False).iloc[0]
+        pass
+
+    def compare(self, test, real):
+        hint = []
+
+        columns_std = ['Genre', 'Rôle', 'Espèce', 'Ressource', 'Portée', 'Régions']
+
+        #Compare each characteristic
+        for col in columns_std:
+            T = set(test[col])
+            R = set(real[col])
+            if T==R:
+                hint.append('g')
+            elif len(set.intersection(T,R))>0:
+                hint.append('o')
+            else:
+                hint.append('r')
+
+        
+        #Compare year
+        if test['Année']==real['Année']:
+            hint.append('=')
+        elif test['Année']>real['Année']:
+            hint.append('-')
+        else:
+            hint.append('+')
+
+
+        return tuple(hint)
+
+
+
+########## Main code ############
+
+#Database : Champion, genre, rôle, espèce, ressource, portée, régions, année
+
+#On récupère notre base de données
+path = __file__[:-9] + "data_lol.csv"
+database = pd.read_csv(path)
+
+
+game = Loldle_Game(database)
+play = True
+
+while play: 
+    game.suggest_play(database)
+    game.update(database)
